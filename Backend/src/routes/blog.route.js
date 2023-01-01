437f76f3eee54e9ae1332fe503346328db33b989
Sathya Swaroop Vandavasi <sathyaswaroopvandavasi@gmail.com>
2023-01-01 13:24:27 +0530

    added more Routes

--- Backend/src/routes/blog.route.js
+++ Backend/src/routes/blog.route.js
@@ -19,4 +19,37 @@ router.use((req, res, next) => {
 router.patch('/update-votes/:name', updatePost);
 router.patch('/update-comment/:name', updateComments);
 
+router.post('/create-post', createPost);
+router.get('/read-post/:name', readPost);
+router.delete('/delete-post/:name', deletePost);
+router.get('/get-all-posts', getAllPosts);
+
+// 
+router.use((req, res, next) => {
+    if (req.user) {
+        next()
+    } else {
+        res.sendStatus(401);
+    }
+})
+
+router.patch('/update-votes/:name', updatePost);
+router.patch('/update-comment/:name', updateComments);
+router.post('/create-post', createPost);
+router.get('/read-post/:name', readPost);
+router.delete('/delete-post/:name', deletePost);
+router.get('/get-all-posts', getAllPosts);
+
+// 
+router.use((req, res, next) => {
+    if (req.user) {
+        next()
+    } else {
+        res.sendStatus(401);
+    }
+})
+
+router.patch('/update-votes/:name', updatePost);
+router.patch('/update-comment/:name', updateComments);
+
 export default router;
